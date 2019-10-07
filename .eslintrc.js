9f24cd02378d857f603115e96bfc89febea5fde2
SeetaRam-Yadav <SeetaRam_Yadav@external.mckinsey.com>
2019-10-08 00:26:35 +0530

    Initial commit.

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "plugins": [],
+  "extends": [
+    "eslint:recommended"
+  ],
+  "parser": "babel-eslint",
+  "rules": {
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"]
+  }
+};
+
